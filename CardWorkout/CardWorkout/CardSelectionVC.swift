b158568821eb2eadee127395c2f6e9f2ab0b2785
Konstantin Krumin <konstantin@borderpass.ca>
2025-03-14 00:08:45 -0400

    Setting up basic logic for card switching and rules UI

--- CardWorkout/CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -11,15 +11,31 @@ class CardSelectionVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
+    var cards: [UIImage] = Card.allValues
+    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        startTimer()
         
         for button in buttons {
             button.layer.cornerRadius = 8
         }
     }
     
+    func startTimer() {
+        timer  = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
     }
 }
