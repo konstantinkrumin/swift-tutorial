e9fb6a6ee0ce835f32755860361daf25719d7968
Konstantin Krumin <konstantin.krumin@intellitix.com>
2025-06-02 23:03:06 -0400

    Init programmatic card workout

---
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,17 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Konstantin Krumin on 2025-06-02.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .brown
+    }
+}
---
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Konstantin Krumin on 2025-06-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
